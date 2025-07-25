bea8b1fec2bb0c1ea3e971ab9ab6de3da3bbda8a
unknown <aboubacar.soumahoro@sonecafrica.com>
2025-07-25 10:03:50 +0000

    Add link to homepage on logo click in Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,7 +35,7 @@ const Hero: React.FC<HeroProps> = ({ id }) => {
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
             >
-              <span>Télécharger maintenant sur play store</span>
+              <span>Télécharger maintenant sur play store gratuitement</span>
               <ArrowRight className="w-5 h-5" />
             </motion.a>
           </motion.div>
